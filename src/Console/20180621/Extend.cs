9596c2100fe5a38ec1090de1b99f58f27fb197e1
Albert <ay_1013@outlook.com>
2018-06-21 12:58:49 +0800

    Init code

---
+++ src/Console/20180621/Extend.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20180621
+{
+    public static class Extend
+    {
+        public static void ExtendTest<T>(T animal)
+        {
+            Mapper.Initialize(x => x.CreateMap<T, Animal>());
+            List<Animal> data = Mapper.Map<List<Animal>>(animal);
+            string strSerializeJSON = JsonConvert.SerializeObject(data);
+            Console.WriteLine(strSerializeJSON);
+
+            string aaaa = JsonConvert.SerializeObject(animal);
+            Console.WriteLine(aaaa);
+
+
+            Console.ReadKey();
+            
+
+        }
+
+        public static void ExtendTest(Animal animal)
+        {
+            Mapper.Initialize(x => x.CreateMap<dynamic, Animal>());
+            List<Animal> data = Mapper.Map<List<Animal>>(animal);
+            string strSerializeJSON = JsonConvert.SerializeObject(data);
+            Console.WriteLine(strSerializeJSON);
+
+            string aaaa = JsonConvert.SerializeObject(animal);
+            Console.WriteLine(aaaa);
+
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+
+
+    public class Animal
+    {
+        public string Eye { set; get; }
+
+        public string Mouth { set; get; }
+
+    }
+
+    public class Person : Animal
+    {
+        public string Speak { set; get; }
+    }
+
+
+}
---
+++ src/Console/20180621/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20180621
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> personList = new List<Person>();
+            personList.Add(new Person()
+            {
+                Eye="黑色",
+                Mouth="一张",
+                Speak="各种语言"
+            });
+            Extend.ExtendTest(personList);
+            Extend.ExtendTest(new Person());
+
+        }
+
+    }
+}
