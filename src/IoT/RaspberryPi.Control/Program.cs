46e5c8f00d1b7a3dcdd41f02682cddd6521f937b
AlbertAY <ay_1013@outlook.com>
2022-07-03 23:47:16 +0800

    update led

--- src/IoT/RaspberryPi.Control/Program.cs
+++ src/IoT/RaspberryPi.Control/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+int pin = 18;
+int lightTime = 1000;
+int dimTime = 200;
+
+using GpioController controller = new();
+controller.OpenPin(pin, PinMode.Output);
+Console.WriteLine($"GPIO pin enabled: {pin}");
+
+while (true)
+{
+    Console.WriteLine($"Light for {lightTime}ms");
+    controller.Write(pin, PinValue.High);
+    Thread.Sleep(lightTime);
+
+    Console.WriteLine($"Dim for {dimTime}ms");
+    controller.Write(pin, PinValue.Low);
+    Thread.Sleep(dimTime);
+}
