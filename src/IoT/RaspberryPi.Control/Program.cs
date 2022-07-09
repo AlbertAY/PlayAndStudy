7304547ef7fc0b412578f6fbd4a3adb3d4169adf
AlbertAY <ay_1013@outlook.com>
2022-07-09 22:19:40 +0800

    1111

--- src/IoT/RaspberryPi.Control/Program.cs
+++ src/IoT/RaspberryPi.Control/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Iot.Device.Bmxx80;
+using System;
 using System.Device.Gpio;
+using System.Device.I2c;
 using System.Threading;
 
 int pin = 18;
@@ -10,13 +12,35 @@ using GpioController controller = new();
 controller.OpenPin(pin, PinMode.Output);
 Console.WriteLine($"GPIO pin enabled: {pin}");
 
-while (true)
-{
-    Console.WriteLine($"Light for {lightTime}ms");
-    controller.Write(pin, PinValue.High);
-    Thread.Sleep(lightTime);
 
-    Console.WriteLine($"Dim for {dimTime}ms");
-    controller.Write(pin, PinValue.Low);
-    Thread.Sleep(dimTime);
-}
+//控制二极管
+Task.Run(() =>
+{
+    while (true)
+    {
+        Console.WriteLine($"Light for {lightTime}ms");
+        controller.Write(pin, PinValue.High);
+        Thread.Sleep(lightTime);
+
+        Console.WriteLine($"Dim for {dimTime}ms");
+        controller.Write(pin, PinValue.Low);
+        Thread.Sleep(dimTime);
+    }
+
+});
+
+//获取温度
+Task.Run(() => 
+{
+    var busId = 1;
+
+    var deviceAddress = 1;
+
+    var aaa = new I2cConnectionSettings(busId, deviceAddress);
+
+    var bbb = I2cDevice.Create(aaa);
+
+    var bme280 = new Bme280(bbb);
+
+});
+
