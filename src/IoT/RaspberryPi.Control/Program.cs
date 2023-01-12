4cb53512259a669abac54d6301b9427f3f68ad2a
aiy <ay_1013@outlook.com>
2023-01-12 16:26:10 +0800

    DHt11获取数据

--- src/IoT/RaspberryPi.Control/Program.cs
+++ src/IoT/RaspberryPi.Control/Program.cs
@@ -40,7 +40,7 @@ namespace RaspberryPi.Control
 
             Console.WriteLine($"Start Get Tem");
 
-            Task.Run(()=>Bme280Start());
+            Task.Run(() => Bme280Start());
 
 
             Console.ReadLine();
@@ -49,6 +49,103 @@ namespace RaspberryPi.Control
 
         }
 
+        static int[] db11Data = { 0, 0, 0, 0, 0 };
+        public static async void DHt11tart()
+        {
+            int pinIndex = 2;
+
+
+
+
+            using GpioController controller = new(PinNumberingScheme.Board);
+
+            controller.OpenPin(pinIndex);
+
+            while (ReadData(controller,pinIndex))
+            {
+                await Task.Delay(1000);
+
+                if(ReadData(controller,pinIndex))
+                {
+                    Console.WriteLine("Read Data Success");
+
+                    Console.WriteLine($"湿度：{db11Data[0]}.{db11Data[0]}%   温度");
+                }
+            }
+        }
+
+
+        public static bool ReadData(GpioController controller, int pinIndex)
+        {
+            Console.WriteLine($"Read Data ........");
+
+            PinValue lastState = PinValue.High;
+
+            int i, j = 0;
+
+            db11Data[0] = db11Data[1] = db11Data[2] = db11Data[3] = db11Data[4] = 0;
+
+            controller.SetPinMode(pinIndex, PinMode.Output);
+
+            controller.Write(pinIndex, 0);
+
+            Thread.Sleep(100);
+
+            controller.Write(pinIndex, 1);
+
+            Thread.Sleep(100);
+
+            controller.SetPinMode(pinIndex, PinMode.Input);
+
+            for (i = 0; i < 85; i++)
+            {
+                int conter = 0;
+
+                while (controller.Read(pinIndex) == lastState)
+                {
+                    conter++;
+
+                    Thread.Sleep(100);
+
+                    if (conter == 100)
+                    {
+                        break;
+                    }
+                }
+
+                lastState = controller.Read(pinIndex);
+
+                if (conter == 100)
+                {
+                    break;
+                }
+
+                if ((i >= 4) && i % 2 == 0)
+                {
+                    db11Data[j / 8] <<= 1;
+
+                    if (conter > 16)
+                    {
+                        db11Data[j / 8] |= 1;
+                    }
+
+                    j++;
+                }
+
+            }
+
+            Console.WriteLine($"End Read ........");
+
+            if ((j >= 40) && (db11Data[4] == ((db11Data[0] + db11Data[1] + db11Data[2] + db11Data[3]) & 0xFF)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
 
 
         public static void Bme280Start()
@@ -58,46 +155,46 @@ namespace RaspberryPi.Control
             {
                 var busId = 1;
 
-            var i2cSettings = new I2cConnectionSettings(busId, Bme280.DefaultI2cAddress);
+                var i2cSettings = new I2cConnectionSettings(busId, Bme280.DefaultI2cAddress);
 
-            using var i2cDevice = I2cDevice.Create(i2cSettings);
+                using var i2cDevice = I2cDevice.Create(i2cSettings);
 
-            var bme280 = new Bme280(i2cDevice);
+                var bme280 = new Bme280(i2cDevice);
 
-            int measurementTime = bme280.GetMeasurementDuration();
+                int measurementTime = bme280.GetMeasurementDuration();
 
 
-            Console.WriteLine($"AAAAAA");
+                Console.WriteLine($"AAAAAA");
 
-            while (true)
-            {                
+                while (true)
+                {
 
-                Console.WriteLine($"BBBBB");
+                    Console.WriteLine($"BBBBB");
 
-                bme280.SetPowerMode(Bmx280PowerMode.Forced);
+                    bme280.SetPowerMode(Bmx280PowerMode.Forced);
 
-                Thread.Sleep(measurementTime);
+                    Thread.Sleep(measurementTime);
 
-                bme280.TryReadTemperature(out var tempValue);
-                bme280.TryReadPressure(out var preValue);
-                bme280.TryReadHumidity(out var humValue);
-                bme280.TryReadAltitude(out var altValue);
+                    bme280.TryReadTemperature(out var tempValue);
+                    bme280.TryReadPressure(out var preValue);
+                    bme280.TryReadHumidity(out var humValue);
+                    bme280.TryReadAltitude(out var altValue);
 
-                Console.WriteLine($"Temperature: {tempValue.DegreesCelsius:0.#}\u00B0C");
-                Console.WriteLine($"Pressure: {preValue.Hectopascals:#.##} hPa");
-                Console.WriteLine($"Relative humidity: {humValue.Percent:#.##}%");
-                Console.WriteLine($"Estimated altitude: {altValue.Meters:#} m");
+                    Console.WriteLine($"Temperature: {tempValue.DegreesCelsius:0.#}\u00B0C");
+                    Console.WriteLine($"Pressure: {preValue.Hectopascals:#.##} hPa");
+                    Console.WriteLine($"Relative humidity: {humValue.Percent:#.##}%");
+                    Console.WriteLine($"Estimated altitude: {altValue.Meters:#} m");
 
-                Thread.Sleep(1000);
+                    Thread.Sleep(1000);
 
-            }
+                }
             }
             catch (System.Exception ex)
             {
-                
-                Console.WriteLine($"Error Message: {0}, DetailInfo : {2}",ex.Message,ex.StackTrace);
+
+                Console.WriteLine($"Error Message: {0}, DetailInfo : {2}", ex.Message, ex.StackTrace);
             }
-            
+
         }
     }
 }
